889e03b896e9b619c7fda0a70681954b74403491
范林松 <781957672@qq.com>
2019-09-15 12:58:42 +0800

    添加 新建、编辑 分类

--- admin/src/views/CategoryEdit.vue
+++ admin/src/views/CategoryEdit.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>新建分类</h1>
+    <h1>{{id?'编辑':'新建'}}新建分类</h1>
     <el-form label-width="120px" @submit.native.prevent="save">
       <el-form-item label="名称">
         <el-input v-model="model.name"></el-input>
@@ -15,6 +15,9 @@
 
 <script>
 export default {
+  props: {
+    id: {}
+  },
   data() {
     return {
       model: {}
@@ -22,15 +25,29 @@ export default {
   },
   methods: {
     async save() {
-      console.log("save....");
-      const res = await this.$http.post("categories", this.model); //将界面数据发送给后端
+      let res;
+      if (this.id) {
+        res = await this.$http.put(`categories/${this.id}`, this.model); //将界面编辑数据发送给后端
+      } else {
+        res = await this.$http.post("categories", this.model); //将界面增加数据发送给后端
+      }
+
       this.$router.push("/categories/list"); //跳转到类型列表界面
       this.$message({
         //使用elementUI自带的提示消息
         type: "success",
         message: "保存成功了"
       });
+    },
+    async fetch() {
+      const res = await this.$http.get(`categories/${this.id}`);
+      console.log(JSON.stringify(res.data));
+      this.model = res.data;
     }
+  },
+
+  created() {
+    this.id && this.fetch();
   }
 };
 </script>
--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -2,9 +2,24 @@ module.exports = app => {
     const express = require('express');
     const router = express.Router();
     const Category = require('../../models/Category');
+    //创建分类
     router.post('/categories', async(req, res) => {
-        console.log("...................2222" + JSON.stringify(req.body));
         const model = await Category.create(req.body); //数据来源于界面url传递过来的数据
+        res.send(model);
+    });
+    //编辑分类
+    router.put('/categories/:id', async(req, res) => {
+        const model = await Category.findByIdAndUpdate(req.params.id, req.body); //数据来源于界面url传递过来的数据
+        res.send(model);
+    });
+    //获取分类列表
+    router.get('/categories', async(req, res) => {
+        const items = await Category.find().limit(10); //数据来源于界面url传递过来的数据
+        res.send(items);
+    });
+    //根据id查询分类
+    router.get('/categories/:id', async(req, res) => {
+        const model = await Category.findById(req.params.id); //数据来源于界面url传递过来的数据
         res.send(model);
     });
     app.use("/admin/api", router);
