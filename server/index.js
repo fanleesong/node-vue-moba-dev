885551cc3622337788ed6511894a943183e30e6d
Peter <fanleesong@gmail.com>
2019-10-12 17:51:33 +0800

    添加处理上传图片问题

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ app.use(express.json()); //使用json数据传输
 require("./routes/admin")(app);
 require("./plugins/db")(app);
 
-app.listen(3000, function (err) {
+app.use('/uploads', express.static(__dirname + '/uploads'));
+
+app.listen(3000, function(err) {
     console.log("connection ........");
 });
