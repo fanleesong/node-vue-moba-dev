b2b19cb2ffefffaf0e38064a5c7c7a544f75e93d
Peter <fanleesong@gmail.com>
2019-10-12 16:55:49 +0800

    添加装备相关的路由列表

--- admin/src/router.js
+++ admin/src/router.js
@@ -4,6 +4,9 @@ import Main from "./views/Main.vue";
 import CategoryEdit from "./views/CategoryEdit.vue";
 import CategoryList from "./views/CategoryList.vue";
 
+import ItemEdit from "./views/ItemEdit.vue";
+import ItemList from "./views/ItemList.vue";
+
 Vue.use(Router);
 
 export default new Router({
@@ -23,6 +26,19 @@ export default new Router({
             {
                 path: "/categories/list",
                 component: CategoryList
+            },
+            {
+                path: "/items/create",
+                component: ItemEdit
+            },
+            {
+                path: "/items/edit/:id",
+                component: ItemEdit,
+                props: true
+            },
+            {
+                path: "/items/list",
+                component: ItemList
             },
         ]
     }]
